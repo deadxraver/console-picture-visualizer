c78f0835613efb35e6b1e55fb99ebdfd16babb27
deadxraver <409853@edu.itmo.ru>
2025-10-15 17:53:20 +0300

    turns out bmp keeps rows in revers order

--- src/main.c
+++ src/main.c
@@ -15,8 +15,8 @@ int main(int argc, char* argv[]) {
     bmp_print_err(res);
     return res;
   }
-  for (size_t i = 0; i < pixels->sz; ++i) {
-    struct list* row = ((struct list**)(pixels->data))[i];
+  for (size_t i = pixels->sz; i > 0; --i) {
+    struct list* row = ((struct list**)(pixels->data))[i-1];
     for (size_t j = 0; j < row->sz; ++j) {
       struct pixel pixel = ((struct pixel*)(row->data))[j];
       MACRO_PRINT_PIXEL(pixel.r, pixel.g, pixel.b);
