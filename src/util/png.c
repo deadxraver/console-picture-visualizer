cd3fdf6af5d44f585165a0f2e71d33f07d64850a
chimi <deadxraver@yandex.ru>
2026-04-17 12:59:47 +0300

    parse png header

--- src/util/png.c
+++ src/util/png.c
@@ -1,11 +1,18 @@
 #include "png.h"
 
+#include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-static uint8_t png_sign[] = {137, 80, 78, 71, 13, 10, 26, 10};
+static const uint8_t png_sign[] = {137, 80, 78, 71, 13, 10, 26, 10};
+
+static const union { uint32_t type; char type_str[4]; }
+  hdr_c = { .type_str = "IHDR" },
+  plte_c = { .type_str = "PLTE" },
+  data_c = { .type_str = "IDAT" },
+  end_c = { .type_str = "IEND" };
 
 void png_print_err(enum png_open_result png_open_result) {
   switch (png_open_result) {
@@ -43,6 +50,10 @@ void png_print_err(enum png_open_result png_open_result) {
 enum png_open_result open_png(char* path, struct list** list_pp) {
   enum png_open_result res = PNG_OK;
   struct png_header header;
+  struct png_chunk chunk;
+  uint32_t crc;
+  char type[5] = {0};
+  uint8_t sign[8];
   int fd = -1;
   if (path == NULL || list_pp == NULL) {
     res = PNG_NULL_PTR;
@@ -53,16 +64,43 @@ enum png_open_result open_png(char* path, struct list** list_pp) {
     res = PNG_FILE_ERR;
     goto end;
   }
-  if (read(fd, &header, sizeof(struct png_header)) < sizeof(struct png_header)) {
+  if (read(fd, sign, sizeof(sign)) < sizeof(sign)) {
     res = PNG_READ_ERR;
     goto end;
   }
   for (size_t i = 0; i < 8; ++i) {
-    if (header.type[i] != png_sign[i]) {
+    if (sign[i] != png_sign[i]) {
       res = PNG_WR_SIGN;
       goto end;
     }
   }
+  if (read(fd, &chunk, sizeof(chunk)) < sizeof(chunk)) {
+    res = PNG_READ_ERR;
+    goto end;
+  }
+  uint32_t length = ntohl(chunk.length);
+  if (length != sizeof(struct png_header)
+    || chunk.type.num != hdr_c.type) {
+    res = PNG_HDR_ERR;
+    goto end;
+  }
+  if (read(fd, &header, length) < length) {
+    res = PNG_READ_ERR;
+    goto end;
+  }
+  if (read(fd, &crc, sizeof(crc)) < sizeof(crc)) {
+    res = PNG_READ_ERR;
+    goto end;
+  }
+  uint32_t width = ntohl(header.width), height = ntohl(header.height);
+  printf("%s: %ux%u\n"
+         "depth: %d, color_type: %d, "
+         "compression_type: %d, filtration_type: %d,\n"
+         "interlace: %d\n",
+         path, (int)width, (int)height,
+         (int)header.bit_depth, (int)header.color_type,
+         (int)header.compression_type, (int)header.filtration_type,
+         (int)header.interlace);
   res = PNG_NO_IMPL;
   // TODO:
 end:
--- src/util/png.h
+++ src/util/png.h
@@ -10,6 +10,7 @@ enum png_open_result {
   PNG_FILE_ERR,           // cannot open file
   PNG_READ_ERR,           // cannot read from file
   PNG_WR_SIGN,            // file has non-bmp signature
+  PNG_HDR_ERR,            // smth wrong with header
   PNG_MALLOC_ERR,         // failed to malloc
   PNG_NULL_PTR,           // one of args is NULL
   PNG_NO_IMPL,            // format not supported yet
@@ -19,8 +20,22 @@ enum png_open_result open_png(char* path, struct list** list_pp);
 
 void png_print_err(enum png_open_result);
 
+struct __attribute__((packed)) png_chunk {
+  uint32_t length;
+  union {
+    uint32_t num;
+    char str[4];
+  } type;
+};
+
 struct __attribute__((packed)) png_header {
-  uint8_t type[8];
+  uint32_t width;
+  uint32_t height;
+  uint8_t bit_depth;
+  uint8_t color_type;
+  uint8_t compression_type;
+  uint8_t filtration_type;
+  uint8_t interlace;
 };
 
 #endif  // !UTIL_PNG_H
